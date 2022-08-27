b88405cf2de73ed57802a6f5f6a279c15659636f
Himanshu <himanshux688@gmail.com>
2022-08-27 13:07:41 +0530

    completed

--- app/src/main/java/com/ansh/diffutil/MainActivity.kt
+++ app/src/main/java/com/ansh/diffutil/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.ansh.diffutil
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -10,19 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-val recyclerView = findViewById<RecyclerView>(R.id.rvProgrammingList)
+        val recyclerView = findViewById<RecyclerView>(R.id.rvProgrammingList)
         val adapter = ProgrammingAdapter()
 
 
-        val p1 = ProgrammingItem(1,"J","Java")
-        val p2 =ProgrammingItem(2,"K","Kotlin")
-        val p3 = ProgrammingItem(3,"A","Android")
+        val p1 = ProgrammingItem(1, "J", "Java")
+        val p2 = ProgrammingItem(2, "K", "Kotlin")
+        val p3 = ProgrammingItem(3, "A", "Android")
 
-        adapter.submitList(listOf(p1,p2,p3))
+        adapter.submitList(listOf(p1, p2, p3))
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = adapter
+
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        btnAdd.setOnClickListener{
+            val p3 = ProgrammingItem(3, "A", "Android")
+            val p4 = ProgrammingItem(4, "R", "Rust")
+            val p5 = ProgrammingItem(5, "G", "GoLang")
+            val p6 = ProgrammingItem(6, "N", "Node")
+
+            adapter.submitList(listOf(p3,p4,p5,p6))
+        }
 
     }
 }
